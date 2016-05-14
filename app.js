b136733ff5100cbbe320a94fe96925ecfb96dede
Paul Kozlovitch <tnk2005@gmail.com>
2016-05-15 00:56:42 +0300

    test

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ var bot = new TelegramBot(token, {polling: true});
 bot.on('message', function (msg) {
     var chatId = msg.chat.id;
     console.log(msg);
-    bot.sendMessage(chatId, "хай йовин", {caption: "I'm a bot!"});
+    bot.sendMessage(chatId, "ololo", {caption: "I'm a bot!"});
 });
