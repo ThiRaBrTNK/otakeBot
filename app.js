c65f5df31c74b6f06a3d9ee376e846012cafbd4c
Paul Kozlovitch <tnk2005@gmail.com>
2016-05-14 16:20:15 +0300

    Updated app tokenization to heroku path vars

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var TelegramBot = require('node-telegram-bot-api');
-var token = require('./token.js');
+var token = process.env.TOKEN;
 var bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', function (msg) {
     var chatId = msg.chat.id;
     console.log(msg);
-    bot.sendMessage(chatId, "Hello!", {caption: "I'm a bot!"});
+    bot.sendMessage(chatId, "хай йовин", {caption: "I'm a bot!"});
 });
