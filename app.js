ec43a6a2e1abbeaa6f6d3a81dd8a4798d36b0721
Paul Kozlovitch <tnk2005@gmail.com>
2016-05-16 22:07:32 +0300

    broadcasting test

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = process.env.TOKEN;
 var bot = new TelegramBot(token, {polling: true});
 
+var chats = [];
+
 bot.on('message', function (msg) {
-    var chatId = msg.chat.id;
+	chats.push(msg.chat.id)
     console.log(msg);
     bot.sendMessage(chatId, "ololo", {caption: "I'm a bot!"});
 });
+
+setInterval(function(){
+	chats.forEach(function(){
+		bot.sendMessage(this, "chat id is " + this);
+	});
+}, 5000)
