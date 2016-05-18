f4d092bd482e70ab7a60e01db05aa66683023625
Paul Kozlovitch <tnk2005@gmail.com>
2016-05-18 23:58:21 +0300

    added: auth request failed handler

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ bot.onText(buildCommandRegExp('start'), function (msg, match) {
 				bot.sendMessage(chatID, 'ЖЕПЬ ЕБРИЛО!!1')
 			}
 		});
+	}).catch(function(){
+		console.log('auth request failed');
 	});
 });
 bot.on('message', function (msg) {
